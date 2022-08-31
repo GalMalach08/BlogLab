9fc0360b1b29b70e770d2ccc3590b3ce7da381ce
Gal Malach <galmalach2@gmail.com>
2022-08-31 12:39:50 +0300

    created identity project with user store

--- BlogLab-API/BlogLab.Repository/IAccountRepository.cs
+++ BlogLab-API/BlogLab.Repository/IAccountRepository.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace BlogLab.Repository
 {
-    public interface AccountRepository
+    public interface IAccountRepository
     {
         public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
         public Task <ApplicationUserIdentity> GetByUsernameAsync (string NormalizedUsername, CancellationToken cancellationToken);
